c2827b485abebd5201d2b41965b9ac881719a05e
lucas-987 <>
2021-03-04 22:51:05 +0100

    Adding vuex

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore} from "vuex";
+import axios from "axios";
+
+const store = createStore({
+    state() {
+        return {
+            cities: [
+
+            ],
+        };
+    }
+});
+
+export default store;
